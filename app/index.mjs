f7091c8f59d21fee4d78c10e86168c0d19463dee
Zimo Li <7163127+lzm0@users.noreply.github.com>
2024-06-12 23:02:27 -0400

    Add random username

--- app/index.mjs
+++ app/index.mjs
@@ -1,10 +1,12 @@
 import process from "process";
 import pty from "node-pty";
 import { WebSocketServer } from "ws";
-import { exec } from "child_process";
+import { execSync, exec } from "child_process";
+import { generateUsername } from "./username-generator.mjs";
 
-const HARD_RESET_INTERVAL = 10 * 60 * 1000;
+const HARD_RESET_INTERVAL = 30 * 60 * 1000;
 const SWEEP_INTERVAL = 30 * 1000;
+const MAX_SESSIONS = 10;
 
 const server = new WebSocketServer({ port: process.env.PORT });
 
@@ -12,14 +14,38 @@ server.on("listening", () => {
   console.log(`Listening on port ${server.address().port}`);
 });
 
-server.on("connection", (socket, req) => {
-  console.log(`Connected to ${req.socket.remoteAddress}`);
+const usernames = new Set();
 
+server.on("connection", (socket, req) => {
+  if (server.clients.size >= MAX_SESSIONS) {
+    socket.send("Maximum number of clients reached\n");
+    socket.terminate();
+    return;
+  }
+
+  let username = generateUsername();
+  while (usernames.has(username)) {
+    username = generateUsername();
+  }
+  usernames.add(username);
+  console.log(`User ${username} connected from ${req.socket.remoteAddress}`);
+
+  execSync(`/usr/bin/docker exec sandbox useradd --create-home ${username}`);
   const term = pty.spawn(
     "/usr/bin/docker",
-    ["exec", "--user", "guest", "-it", "sandbox", "/bin/bash"],
+    [
+      "exec",
+      "--user",
+      username,
+      "--workdir",
+      `/home/${username}`,
+      "-it",
+      "sandbox",
+      "/bin/bash",
+    ],
     { name: "xterm-color" }
   );
+  term.username = username;
   socket.isAlive = true;
   socket.term = term;
   term.onData((data) => socket.send(data));
@@ -27,14 +53,14 @@ server.on("connection", (socket, req) => {
   socket.on("message", (data) => term.write(data));
 
   socket.on("close", () => {
-    console.log(`Disconnected from ${req.socket.remoteAddress}`);
-    term.kill();
+    kill(term);
+    console.log(`User ${username} disconnected`);
   });
 
   socket.on("pong", heartbeat);
 
   socket.on("error", (error) => {
-    term.kill();
+    kill(term);
     console.error(error);
   });
 });
@@ -44,9 +70,8 @@ const sweepTask = setInterval(sweep, SWEEP_INTERVAL);
 const hardResetTask = setInterval(() => {
   server.clients.forEach((socket) => {
     socket.terminate();
-    socket.term.kill();
+    kill(socket.term);
   });
-  exec("/usr/bin/docker exec sandbox pkill -9 -u guest");
 }, HARD_RESET_INTERVAL);
 
 server.on("close", () => {
@@ -58,11 +83,23 @@ function heartbeat() {
   this.isAlive = true;
 }
 
+function kill(term) {
+  term.kill();
+  exec(`/usr/bin/docker exec sandbox pkill -9 -u ${term.username}`, () => {
+    exec(
+      `/usr/bin/docker exec sandbox userdel --remove ${term.username}`,
+      () => {
+        usernames.delete(term.username);
+      }
+    );
+  });
+}
+
 function sweep() {
   server.clients.forEach((socket) => {
     if (socket.isAlive === false) {
       socket.terminate();
-      socket.term.kill();
+      kill(socket.term);
     }
     socket.isAlive = false;
     socket.ping();
