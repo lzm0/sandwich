33207c082bee25965aca13b473f52a5fb1652821
Zimo Li <7163127+lzm0@users.noreply.github.com>
2024-06-15 16:36:46 -0400

    Remove existing "guest" user before adding a new one

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+if id "guest" > /dev/null 2>&1; then
+  userdel -rf guest > /dev/null 2>&1
+fi
 useradd --create-home --uid 1001 guest
 cp WELCOME.md /home/guest/README.md
 chmod 644 /home/guest/README.md
